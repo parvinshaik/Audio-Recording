43981902ee9c498c1254632ecd1520513922fbf5
ParvinShaik <156885514+parvinshaik@users.noreply.github.com>
2025-05-01 16:18:37 +0530

    Add files via upload

---
+++ TestTakerDashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AudioRecorder from '../components/AudioRecorder';
+
+const TestTakerDashboard: React.FC = () => {
+  return (
+    <div className="dashboard">
+      <h1>Test Taker Dashboard</h1>
+      <AudioRecorder />
+    </div>
+  );
+};
+
+export default TestTakerDashboard;

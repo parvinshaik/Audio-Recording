9d0c442c4d391d8c06c218dc24d8a1bf92436a9e
ParvinShaik <156885514+parvinshaik@users.noreply.github.com>
2025-05-01 16:15:28 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TestTakerDashboard from './pages/TestTakerDashboard';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <TestTakerDashboard />
+    </div>
+  );
+};
+
+
+
+
+export default App;
